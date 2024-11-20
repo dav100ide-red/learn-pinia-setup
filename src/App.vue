6d1385cf3458b8c1457c8ca00564888a837dfd84
davideoreglio <doreglio@red.software.systems>
2024-11-20 15:01:04 +0100

    v-for for links in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="app">
         <nav>
-            <RouterLink to="/">Home</RouterLink> |
-            <RouterLink to="/about">About</RouterLink>
+            <span v-for="(link, index) in links" :key="index">
+                <RouterLink :to="link.path">{{ link.label }}</RouterLink>
+                <span v-if="index < links.length - 1"> | </span>
+            </span>
         </nav>
         <RouterView />
     </div>
 </template>
+
+<script setup lang="ts">
+const links = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Min & Max', path: '/min-max' },
+]
+</script>
 
 <style scoped>
 .app {

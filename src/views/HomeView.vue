555f54688e22e215d7b05795fc140ab342dae632
davideoreglio <doreglio@red.software.systems>
2024-11-20 11:01:18 +0100

    add count red to homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <h1>0</h1>
+        <h1>{{ count }}</h1>
         <div class="buttons">
             <button>-</button>
             <button>+</button>
@@ -8,10 +8,14 @@
     </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
 <style scoped lang="scss">
 h1,
 button {
     font-size: 50px;
 }
-
 </style>

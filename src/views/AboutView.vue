7cf5e1db4bb7b460b28f151292f28bc2d67b1618
davideoreglio <doreglio@red.software.systems>
2024-11-21 10:45:14 +0100

    fix About view vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="about">
         <h1>About</h1>
-        <button class="btn-primary" @click="counterStore.increaseCount()">
-            {{ counterStore.count }}
+        <button
+            class="btn-primary"
+            @click="counterStore.setCount(count + 1)"
+            :disabled="count >= max">
+            {{ count }}
         </button>
     </div>
 </template>
 
 <script setup>
-import { useCounterStore } from '@/stores/counter'
-const counterStore = useCounterStore()
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
+const counterStore = useCounterStore();
+const { count, max } = storeToRefs(counterStore);
 </script>

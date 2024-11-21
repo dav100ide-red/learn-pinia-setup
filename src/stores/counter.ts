1688d0d1e8a861f942dd9e850e05a11d09c7d768
davideoreglio <doreglio@red.software.systems>
2024-11-21 11:03:45 +0100

    add counterState

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,9 +1,10 @@
 import { computed, reactive } from 'vue';
 import { defineStore } from 'pinia';
+import { CounterState } from '../types/counter.type';
 
 export const useCounterStore = defineStore('counter', () => {
     //STATE
-    const state = reactive({ count: 0, min: -15, max: 30 });
+    const state = reactive<CounterState>({ count: 0, min: -15, max: 30 });
 
     function setCount(amount = 1) {
         if (amount <= state.max && amount >= state.min) {
@@ -11,11 +12,11 @@ export const useCounterStore = defineStore('counter', () => {
         }
     }
 
-    //computed, refac andrea
     const oddOrEven = computed(() => (state.count % 2 === 0 ? 'even' : 'odd'));
-    const count = computed(() => state.count);
-    const min = computed(() => state.min);
-    const max = computed(() => state.max);
+    //qual'e lo scopo di dichiarare i seguenti computed?
+    const count = computed<number>(() => state.count);
+    const min = computed<number>(() => state.min);
+    const max = computed<number>(() => state.max);
 
     return {
         count,

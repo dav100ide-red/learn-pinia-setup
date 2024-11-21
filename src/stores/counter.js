2c737401678238ae0d6000a745ababd1dbd5b212
davideoreglio <doreglio@red.software.systems>
2024-11-21 10:48:54 +0100

    counter.js remove useless comments

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,9 +4,6 @@ import { defineStore } from 'pinia';
 export const useCounterStore = defineStore('counter', () => {
     //STATE
     const state = reactive({ count: 0, min: -15, max: 30 });
-    // const count = ref(0);
-    // const min = ref(-15);
-    // const max = ref(30);
 
     function setCount(amount = 1) {
         if (amount <= state.max && amount >= state.min) {
@@ -14,26 +11,19 @@ export const useCounterStore = defineStore('counter', () => {
         }
     }
 
-    // function decreaseCount(decreaseAmount = 1) {
-    //     if (state.count - decreaseAmount >= state.min) {
-    //         state.count = state.count - decreaseAmount;
-    //     }
-    // }
-
-    //computed
+    //computed, refac andrea
     const oddOrEven = computed(() => (state.count % 2 === 0 ? 'even' : 'odd'));
     const count = computed(() => state.count);
     const min = computed(() => state.min);
     const max = computed(() => state.max);
 
     return {
-        //STATE
         count,
+
         min,
         max,
         oddOrEven,
 
         setCount,
-        //decreaseCount,
     };
 });
